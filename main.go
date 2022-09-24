2deeb9bc2da374173b0e30dab9299cb2d8b7ebc1
Joe Cui <xiaozhou.cui@gmail.com>
2022-09-24 11:47:53 +1000

    Use slice to store bookings

--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	const conferenceTickets int = 50
-	var remainingTickets uint = 50
-	conferenceName := "Go Conference" // shortcut for var definition
+	const conferenceTickets int = 50  // const cannot be changed
+	var remainingTickets uint = 50    // var can be changed
+	conferenceName := "Go Conference" // shortcut for var definition (N/A for const)
+	var bookings = []string{}         // empty slice of flexible size
 
 	// print variable types
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
@@ -17,7 +18,7 @@ func main() {
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 
 	// ask user input for their details
 	fmt.Println("Please enter your first name: ")
@@ -32,5 +33,12 @@ func main() {
 	fmt.Println("Please enter number of tickets: ")
 	fmt.Scan(&userTickets)
 
+	remainingTickets = remainingTickets - userTickets
+
+	bookings = append(bookings, firstName+" "+lastName)
+
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v.\n", remainingTickets, conferenceName)
+
+	fmt.Printf("These are all our bookings %v\n", bookings)
 }
