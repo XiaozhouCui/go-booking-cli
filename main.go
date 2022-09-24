f8dc99219f69a9d629ba3aa93392b5f041924424
Joe Cui <xiaozhou.cui@gmail.com>
2022-09-24 10:39:58 +1000

    Get user input with pointer

--- main.go
+++ main.go
@@ -14,10 +14,23 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
 
-	userName = "Tom"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	// ask user input for their details
+	fmt.Println("Please enter your first name: ")
+	fmt.Scan(&firstName) // "&firstName" is a pointer, which is memory address like 0xc000014270
+
+	fmt.Println("Please enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Please enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Please enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
 }
