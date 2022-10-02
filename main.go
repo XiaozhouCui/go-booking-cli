ae5c37a1d0cbc593f1001d858f6d6f0611a54c0f
Joe Cui <xiaozhou.cui@gmail.com>
2022-10-02 13:01:38 +1000

    Extract and reuse package-level variables

--- main.go
+++ main.go
@@ -5,39 +5,24 @@ import (
 	"strings"
 )
 
-func main() {
-	const conferenceTickets int = 50  // const cannot be changed
-	var remainingTickets uint = 50    // var can be changed
-	conferenceName := "Go Conference" // shortcut for var definition with type inference (N/A for const)
-	var bookings = []string{}         // empty slice of flexible size
+// package-level variables, shared across funcs
+const conferenceTickets int = 50            // const cannot be changed
+var remainingTickets uint = 50              // var can be changed
+var conferenceName string = "Go Conference" // cannot use shortcut declaration (a := b) in package-level variables
+var bookings = []string{}                   // empty slice of flexible size
 
-	greetUsers(conferenceName, conferenceTickets, remainingTickets)
+func main() {
+
+	greetUsers()
 
 	// print variable types
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
 	// indefinite loop, each loop for each user
 	for {
-		var firstName string
-		var lastName string
-		var email string
-		var userTickets uint
-
-		// ask user input for their details
-		fmt.Println("Please enter your first name: ")
-		fmt.Scan(&firstName) // "&firstName" is a pointer, which is memory address like 0xc000014270
-
-		fmt.Println("Please enter your last name: ")
-		fmt.Scan(&lastName)
-
-		fmt.Println("Please enter your email address: ")
-		fmt.Scan(&email)
-
-		fmt.Println("Please enter number of tickets: ")
-		fmt.Scan(&userTickets)
-
-		// go func can return multi values
-		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets, remainingTickets)
+		// go functions can return multiple values
+		firstName, lastName, email, userTickets := getUserInput()
+		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets)
 
 		// // if email is invalid, use "continue" to skip the remainder of current loop, retry booking with another loop
 		// if !isValidEmail {
@@ -46,20 +31,16 @@ func main() {
 		// }
 
 		if isValidName && isValidEmail && isValidTicketNumber {
-			remainingTickets = remainingTickets - userTickets
 
-			bookings = append(bookings, firstName+" "+lastName)
-
-			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
-			fmt.Printf("%v tickets remaining for %v.\n", remainingTickets, conferenceName)
+			bookTicket(userTickets, firstName, lastName, email)
 
 			// firstNames is a slice of strings
-			firstNames := getFirstNames(bookings)
+			firstNames := getFirstNames()
 			fmt.Printf("The first names of bookings are: %v\n", firstNames)
 
 			if remainingTickets == 0 {
 				// end program (loop)
-				fmt.Println("Our conference is booked out. Come back next year.")
+				fmt.Println("Our conference is booked out. Come back next year")
 				break
 			}
 		} else {
@@ -71,19 +52,19 @@ func main() {
 			}
 			if !isValidTicketNumber {
 				fmt.Println("Number of tickets you entered is invalid")
-				fmt.Printf("We only have %v tickets remaning, so you can't book %v tickets\n", remainingTickets, userTickets)
 			}
 		}
 	}
 }
 
-func greetUsers(confName string, confTickets int, remainingTickets uint) {
-	fmt.Printf("Welcome to %v booking application\n", confName)
-	fmt.Printf("We have total of %v tickets and %v are still available\n", confTickets, remainingTickets)
+func greetUsers() {
+	// using package-level variables directly, without parameters
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 }
 
-func getFirstNames(bookings []string) []string {
+func getFirstNames() []string {
 	firstNames := []string{} // shortcut for creating an empty slice
 	// loop through bookings slice, "_" is for unused param (index in this case)
 	for _, booking := range bookings {
@@ -95,10 +76,42 @@ func getFirstNames(bookings []string) []string {
 	return firstNames
 }
 
-func validateUserInput(firstName string, lastName string, email string, userTickets uint, remainingTickets uint) (bool, bool, bool) {
+func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
 	isValidName := len(firstName) >= 2 && len(lastName) >= 2
 	isValidEmail := strings.Contains(email, "@")
+	// remainingTickets is package-level variable
 	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
 	// golang can return multiple values
 	return isValidName, isValidEmail, isValidTicketNumber
+}
+
+func getUserInput() (string, string, string, uint) {
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	// ask user input for their details
+	fmt.Println("Please enter your first name: ")
+	fmt.Scan(&firstName) // "&firstName" is a pointer, which is memory address like 0xc000014270
+
+	fmt.Println("Please enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Please enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Please enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	return firstName, lastName, email, userTickets
+}
+
+func bookTicket(userTickets uint, firstName string, lastName string, email string) {
+	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, firstName+" "+lastName)
+	fmt.Printf("bookings: %v \n", bookings)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
